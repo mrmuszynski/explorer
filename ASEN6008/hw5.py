6fcea8e11322d25ebdd30950df95902c02f82bc9
Matt <mrmuszynski@gmail.com>
2018-03-13 07:36:25 -0600

    final commit of hw5

--- ASEN6008/hw5.py
+++ ASEN6008/hw5.py
@@ -15,9 +15,11 @@ sys.path.insert(0, '../fsw')
 sys.path.insert(0, '../../lib')
 
 import vehicles, celestialBodies, simScenario
+from analysisTools import computeB, dBdV
+
 from numpy import linspace, hstack, pi, array, arccos, cos, cross
 from numpy import rad2deg, arctan2, vstack, sqrt, logspace
-from numpy.linalg import norm
+from numpy.linalg import norm, inv
 import matplotlib.pyplot as plt
 from timeFcn import timeConvert, sec2day, day2sec
 from util import r3
@@ -34,27 +36,6 @@ import orbits as o
 earth = celestialBodies.celestialBody()
 earth.initEarth()
 
-def computeB(r,v):
-	rP = o.rv2coe(r,v)['r_p']
-	s = v/norm(v)
-	k = array([0,0,1])
-	t = cross(s,k)/norm(cross(s,k))
-	h = cross(r,v)/norm(cross(r,v))
-	rhat = cross(s,t)
-	bHat = cross(s,h)
-	b = earth.mu/v.dot(v)*sqrt((1+v.dot(v)*(rP/earth.mu))**2-1)
-	ijk2str = vstack([s,t,rhat])
-	return (b*bHat,ijk2str)
-
-def dBdV(vNominal,vPerturbed,r):
-	bNominal = computeB(r,vNominal)[0]
-	ijk2strNominal = computeB(r,vPerturbed)[1]
-	bPerturbed = computeB(r,vPerturbed)[0]
-	if sum((vNominal - vPerturbed) == 0) < 2: 
-		print('WARNING: Only one element of vInf can be varied!')
-	dBdVijk = (bPerturbed - bNominal)/norm(vNominal - vPerturbed)
-	return ijk2strNominal.dot(dBdVijk)
-
 rSOI = array([546507.344255845, -527978.380486028, 531109.066836708])
 vSOI = array([-4.9220589268733, 5.36316523097915, -5.22166308425181])
 
@@ -66,20 +47,52 @@ dBRdVx = []
 dBRdVy = []
 for perturbation in perturbations:
 	v = vSOI + array([perturbation,0,0])
-	partial = dBdV(vSOI,v,rSOI)
+	partial = dBdV(vSOI,v,rSOI,earth.mu)
 	dBTdVx = hstack([dBTdVx,partial[1]])
 	dBRdVx = hstack([dBRdVx,partial[2]])
 	v = vSOI + array([0,perturbation,0])
-	partial = dBdV(vSOI,v,rSOI)
+	partial = dBdV(vSOI,v,rSOI,earth.mu)
 	dBTdVy = hstack([dBTdVy,partial[1]])
 	dBRdVy = hstack([dBRdVy,partial[2]])
 
 plt.figure()
-plt.semilogx(perturbations,dBTdVx,label=r'$\frac{\partial B_T}{\partial V_x}$')
-plt.semilogx(perturbations,dBTdVy,label=r'$\frac{\partial B_T}{\partial V_y}$')
-plt.semilogx(perturbations,dBRdVx,label=r'$\frac{\partial B_R}{\partial V_x}$')
-plt.semilogx(perturbations,dBRdVy,label=r'$\frac{\partial B_R}{\partial V_y}$')
-plt.legend()
+plt.semilogx(perturbations,dBTdVx,label=r'$\partial B_T/ \partial V_{\infty,x}$')
+plt.semilogx(perturbations,dBTdVy,label=r'$\partial B_T/ \partial V_{\infty,y}$')
+plt.semilogx(perturbations,dBRdVx,label=r'$\partial B_R/ \partial V_{\infty,x}$')
+plt.semilogx(perturbations,dBRdVy,label=r'$\partial B_R/ \partial V_{\infty,y}$')
+plt.title(r'Partials of $\vec{B}$ with respect to $\vec{v}_\infty$')
+plt.ylabel(r'Partial Derivative ($\frac{km}{km/s}$)')
+plt.xlabel(r'Deviation in $V_{\infty,i}$ ($\frac{km}{s}$)')
+plt.legend(bbox_to_anchor=(0.1,0.2))
+
+###############################################################################
+#
+# 	Problem 2
+#
+###############################################################################
+
+r = rSOI
+v = array(vSOI)
+pert = 1e-10
+bDesired = array([0,13135.7982982557,5022.26511510685])
+
+normDeltaB = 1
+while normDeltaB > 1e-6:
+	b = computeB(r,v,earth.mu)
+	bNomstr = b[1].dot(b[0])
+	dBdVx = dBdV(v,v + array([pert,0,0]),r,earth.mu)
+	dBdVy = dBdV(v,v + array([0,pert,0]),r,earth.mu)
+	bMatrix = vstack([dBdVx[1:3],dBdVy[1:3]]).T
+	bInv = inv(bMatrix)
+	deltaB = (bDesired - bNomstr)[1:3]
+	deltaV = hstack([bInv.dot(deltaB),0]) #in STR frame?
+	v += deltaV
+	normDeltaB = norm(deltaB)
+
+deltaVFinal = v - vSOI
+print('Final Delta V: ' + str(deltaVFinal))
+print('Final V: ' + str(v))
+
 
 pdb.set_trace()
 
